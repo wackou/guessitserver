96166f9393353d724f7ae5ee00abd679d9aae29d
Nicolas Wack <wackou@gmail.com>
2014-12-28 11:56:36 +0100

    Added AGPLv3+ license

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+# GuessItServer - Providing http access points to GuessIt
+# Copyright (c) 2014 Nicolas Wack <wackou@gmail.com>
+#
+# GuessItServer is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# GuessItServer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 from setuptools import setup, find_packages
 import os.path
@@ -26,10 +42,13 @@ args = dict(name='guessitserver',
             # Get strings from
             # http://pypi.python.org/pypi?%3Aaction=list_classifiers
             classifiers=['Development Status :: 2 - Pre-Alpha',
-                         'License :: Other/Proprietary License',
+                         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
                          'Operating System :: OS Independent',
                          'Programming Language :: Python :: 2',
                          'Programming Language :: Python :: 2.7',
+                         'Programming Language :: Python :: 3',
+                         'Programming Language :: Python :: 3.3',
+                         'Programming Language :: Python :: 3.4'
                          ],
             keywords='guessit server',
             author='Nicolas Wack',
